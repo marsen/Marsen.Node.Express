b9088fd230594c9727ed7c0a4a025f0136d40636
lin <thisismysoul@gmail.com>
2016-08-20 03:02:06 +0800

    modify app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,11 +11,20 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', routes);
@@ -27,7 +37,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-//================
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
