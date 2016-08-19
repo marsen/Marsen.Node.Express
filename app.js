62cd8568ea7609574eebb527a01fe5daa1370843
lin <thisismysoul@gmail.com>
2016-08-20 02:48:10 +0800

    add app.js

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-/**
- * Created by atulr on 05/07/15.
- */
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var routes = require('./routes/index');
+var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,13 +41,16 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-//================
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('error'+ err.message );
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
 }
 
@@ -41,7 +58,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error'+ err.message );
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
