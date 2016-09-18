ecc67ce0be9429bafdbddd49be7ca51492c14c68
lin <thisismysoul@gmail.com>
2016-09-18 19:52:26 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//redirect domain to www.domain
+router.all(/.*/, function(req, res, next) {
+  var host = req.hostname;
+  console.log(host);
 
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "http://www." + host);
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//redirect domain to www.domain
+router.all(/.*/, function(req, res, next) {
+  var host = req.hostname;
+  console.log(host);
+
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "http://www." + host);
+  }
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
