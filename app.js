ae830eccb7e74dcebb87b05d94439f51f1fc38a6
lin <thisismysoul@gmail.com>
2016-09-18 16:13:50 +0800

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var about = require('./routes/about');
 var app = express();
 //redirect domain to www.domain
 app.all(/.*/, function(req, res, next) {
-  consolo.log("\r\n app is :"+req.app);
+  consolo.log("\r\n app is :"+ req.hostname );
   
   next();
 });
