6259b3af74b666157d057d6e55b62e758368acd7
lin <thisismysoul@gmail.com>
2016-09-18 21:38:03 +0800

    add style.css

--- routes/about.js
+++ routes/about.js
@@ -1,18 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-//redirect domain to www.domain
-router.all('*', function(req, res, next) {
-  var host = req.hostname;
-  console.log(host);
-
-  if (host.match(/^www\..*/i)) {
-    next();
-  } else {
-    res.redirect(301, "http://www." + req.hostname + "/" + req.path);
-  }
-});
-
 /* GET about page. */
 router.get(['/','/me'], function(req, res, next) {
   res.render('about/me', { title: '關於我' });
