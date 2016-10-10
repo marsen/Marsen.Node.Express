2ccde6a7634e5d5b8dc3bfd6787bf8d8d65ee6e9
marklin <marklin@nine-yi.com>
2016-10-10 17:58:43 +0800

    KATA TODOLIST - 建立BaseService

--- public/javascripts/kata/todo.ts
+++ public/javascripts/kata/todo.ts
@@ -2,84 +2,38 @@
 /// <reference path="../../../node_modules/@types/bootstrap/index.d.ts"/>
 /// <reference path="../../../node_modules/@types/jqueryui/index.d.ts"/>
 
-$("#sortable").sortable();
-$("#sortable").disableSelection();
-
-countTodos();
-
-// all done btn
-$("#checkAll").click(function(){
-    AllDone();
-});
-
-//create todo
-$('.add-todo').on('keypress',function (e) {
-      e.preventDefault
-      if (e.which == 13) {
-           if($(this).val() != ''){
-           var todo = $(this).val();
-            createTodo(todo); 
-            countTodos();
-           }else{
-               // some validation
-           }
-      }
-});
-// mark task as done
-$('.todolist').on('change','#sortable li input[type="checkbox"]',function(){
-    if($(this).prop('checked')){
-        var doneItem = $(this).parent().parent().find('label').text();
-        $(this).parent().parent().parent().addClass('remove');
-        done(doneItem);
-        countTodos();
-    }
-});
-
-//delete done task from "already done"
-$('.todolist').on('click','.remove-item',function(){
-    removeItem(this);
-});
-
-// count tasks
-function countTodos(){
-    var countNum = $("#sortable li").length;
-    $('.count-todos').html(countNum.toString());
-}
-
-//create task
-function createTodo(text){
-    var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
-    $('#sortable').append(markup);
-    $('.add-todo').val('');
-}
-
-//mark task as done
-function done(doneItem){
-    var done = doneItem;
-    var markup = '<li>'+ done +'<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
-    $('#done-items').append(markup);
-    $('.remove').remove();
-}
-
-//mark all tasks as done
-function AllDone(){
-    var myArray = [];
-
-    $('#sortable li').each( function() {
-         myArray.push($(this).text());   
-    });
+class BaseService<T>{
+    constructor(private type: string) {}
     
-    // add to done
-    for (var i = 0; i < myArray.length; i++) {
-        $('#done-items').append('<li>' + myArray[i] + '<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
+    List = new  Array<T>() ;
+
+    Create(data: T) {
+        
     }
-    
-    // myArray
-    $('#sortable li').remove();
-    countTodos();
+
+    Update(date: T){
+
+    }
+
+    Delete(data: T){
+
+    }
+
+    GetAll(): Array<T> {
+         return this.List;
+    }
+
+    Render(){
+
+    }
 }
 
-//remove done task from list
-function removeItem(element){
-    $(element).parent().remove();
+interface todoItem {
+    Content: string;
+    Status: todoStatus;
+}
+
+enum todoStatus{
+    undo,
+    done,    
 }
