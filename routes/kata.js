7d141faded63f496d34489aeb59c2babad5a569f
marklin <marklin@nine-yi.com>
2016-10-13 19:10:08 +0800

    KATA TODOLIST - 加入KATA首頁

--- routes/kata.js
+++ routes/kata.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET about page. */
-router.get(['/','/todo'], function(req, res, next) {
+router.get('/', function(req, res, next) {
+  res.render('kata/index', { title: 'KATA!!' });
+});
+router.get('/todo', function(req, res, next) {
   res.render('kata/todo', { title: 'todo' });
 });
 
