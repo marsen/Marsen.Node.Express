2ab1551be6a58f7d22b4719d22cac2076b1c442a
marklin <marklin@nine-yi.com>
2016-10-20 10:56:24 +0800

    add apiindex 防呆

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var phrases = ['aa','bb'];
+//var phares = require('./utils/phares');
 /* GET about page. */
 router.get('/', function(req, res, next) {
-  res.send('aa');
+  var phares =
+ [  'Life without a friend is death.',
+    'Kings go mad， and the people suffer for it.',
+    'Action speak louder than words.',
+    'Bad times make a good man.',
+    'Sharp tools make good work.',
+    'Never put off what you can do today until tomorrow'           
+             ];
+  var randex = ~~((phares.length-0) * Math.random())+0 ;
+  console.log("random number is : " + randex);           
+  res.send(phares[randex]);
 });
 
 
