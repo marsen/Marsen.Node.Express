0f713bd9c2eef0e5ab391261845b50682aee8dc4
marklin <marklin@nine-yi.com>
2018-09-25 16:33:53 +0800

    Welcomm To 3arsen

--- typescript/routes/index.ts
+++ typescript/routes/index.ts
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req:core.Request, res:core.Response, next:core.NextFunction)=> {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcomm To 3arsen ' });
 });
 
 module.exports = router;
