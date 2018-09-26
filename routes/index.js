25bcd994ce7356efa427344c912706ab51687464
marklin <marklin@nine-yi.com>
2018-09-26 13:13:28 +0800

    run gulp

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Welcomm To 3arsen ' });
 });
 module.exports = router;
